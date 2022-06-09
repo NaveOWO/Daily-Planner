6787be883c6474824c0ef5b4c59e86fe5bdfd067
JY <pianowell96@gmail.com>
2022-06-10 01:25:02 +0900

    [feat]: input생성기능 구현

--- src/components/DailyCheck.js
+++ src/components/DailyCheck.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const StyleRoot = styled.section`
@@ -9,28 +10,82 @@ const DailyCheckContainer = styled.section`
   margin-top: 30px;
 `;
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const DailyTitle = styled.h2`
   font-size: 20px;
   font-family: "Independence_hall";
   color: white;
   margin: 0;
-  padding: 10px 0 0 40px;
+  padding: 10px 0 10px 40px;
 `;
 
-const DailyContents = styled.input`
-  height: 80%;
-  width: 99%;
+const ListPlusBtn = styled.button`
+  color: white;
+  background-color: transparent;
+  border: none;
+  font-size: 30px;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const DailyContents = styled.ul`
+  height: 70%;
+  /* width: 99%; */
   padding: 0;
+  margin: 0;
   border: none;
   background-color: transparent;
 `;
 
+const DailyCheckList = styled.li`
+  width: 80%;
+  margin-left: 40px;
+  border: none;
+  display: flex;
+`;
+
+const ListMarker = styled.span``;
+
+const DailyCheckInput = styled.input`
+  width: 100%;
+  font-size: 20px;
+  font-family: "ParkYongJun";
+  border: none;
+  background-color: transparent;
+  padding: 0 0 0 10px;
+  &:focus {
+    outline: none;
+  }
+`;
+
 const DailyCheck = () => {
+  const [checkList, setCheckList] = useState([]);
+
+  const clickHandle = () => setCheckList([...checkList, ""]);
+
   return (
     <StyleRoot>
       <DailyCheckContainer>
-        <DailyTitle>Daily Check</DailyTitle>
-        <DailyContents />
+        <TitleContainer>
+          <DailyTitle>Daily Check</DailyTitle>
+          <ListPlusBtn onClick={clickHandle}>+</ListPlusBtn>
+        </TitleContainer>
+        <DailyContents>
+          <DailyCheckList>
+            <ListMarker>✔</ListMarker>
+            <DailyCheckInput />
+          </DailyCheckList>
+          {checkList.map((list, index) => (
+            <DailyCheckList key={index}>
+              <ListMarker>✔</ListMarker>
+              <DailyCheckInput />
+            </DailyCheckList>
+          ))}
+        </DailyContents>
       </DailyCheckContainer>
     </StyleRoot>
   );
--- src/components/Priorities.js
+++ src/components/Priorities.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { useRef } from "react";
+import { click } from "@testing-library/user-event/dist/click";
+import { useEffect } from "react";
 
 const StyleRoot = styled.section`
   background-color: rgb(255, 255, 255, 0.5);
@@ -9,12 +13,26 @@ const PrioritiesContainer = styled.section`
   height: 250px;
 `;
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const PrioritiesTitle = styled.h2`
   font-size: 20px;
   font-family: "Independence_hall";
   color: white;
   margin: 0;
   padding: 10px 0 10px 40px;
+`;
+
+const ListPlusBtn = styled.button`
+  color: white;
+  background-color: transparent;
+  border: none;
+  font-size: 30px;
+  margin-right: 20px;
+  cursor: pointer;
 `;
 
 const PrioritiesContents = styled.ul`
@@ -42,18 +60,44 @@ const PrioritiesInput = styled.input`
   border: none;
   background-color: transparent;
   padding: 0 0 0 10px;
+  margin-bottom: 10px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const PrioritiesText = styled.div`
+  width: 100%;
+  font-size: 20px;
+  font-family: "ParkYongJun";
+  background-color: transparent;
+  padding: 0 0 0 10px;
+  margin-bottom: 10px;
 `;
 
 const Priorities = () => {
+  const [priorityList, setPriorityList] = useState([]);
+  let listCnt = 0;
+
+  const clickHandle = () => setPriorityList([...priorityList, ""]);
   return (
     <StyleRoot>
       <PrioritiesContainer>
-        <PrioritiesTitle>Priorities</PrioritiesTitle>
+        <TitleContainer>
+          <PrioritiesTitle>Priorities</PrioritiesTitle>
+          <ListPlusBtn onClick={clickHandle}>+</ListPlusBtn>
+        </TitleContainer>
         <PrioritiesContents>
           <PrioritiesList>
             <ListMarker>🌙</ListMarker>
             <PrioritiesInput />
           </PrioritiesList>
+          {priorityList.map((list, index) => (
+            <PrioritiesList key={index}>
+              <ListMarker>🌙</ListMarker>
+              <PrioritiesInput />
+            </PrioritiesList>
+          ))}
         </PrioritiesContents>
       </PrioritiesContainer>
     </StyleRoot>
